d70a30db0918f22e392393b3d843e3a5f9afa01e
Kevin Nguyen <thenoc27@gmail.com>
2022-12-30 21:29:30 -0500

    Stuck on the arithmetic section

--- app.js
+++ app.js
@@ -28,22 +28,128 @@ const currentOutputEl = document.querySelector('.current-output');
 const clearBtn = document.getElementById('clear-btn');
 const deleteBtn = document.getElementById('delete-btn');
 const decimalBtn = document.getElementById('decimal-btn');
+let decimal_btn_enabled = true;
+
 const calculateBtn = document.getElementById('equal-btn');
 const digitBtns = document.querySelectorAll('.digit-btn');
 const arithmeticBtns = document.querySelectorAll('.arithmetic-btn');
 
-let firstNum = "";
-let secondNum = "";
+// const modulusBtn = document.getElementById('modulus-btn');
+// const divideBtn = document.getElementById('division-btn');
+// const multiplyBtn = document.getElementById('multiplication-btn');
+// const subtractBtn = document.getElementById('subtraction-btn');
+// const addBtn = document.getElementById('addition-btn');
+
+let userNum = "";
+let total = "";
+let previousOperator;
 let currentOperator;
 
-// Count the number in the javascript and then visually count the number on
-// the webpage
+// Have the math icons and numbers here for the visual, but 
+// Have the actual numbers and operators in other variables
+let equation = "";
 
+/*
+- Once we press an arithmetic we store that first number. If we plus an arithmetic
+we check if a number has already been stored, if a number was already stored then we 
+know that we should combine the numbers in some way
+*/
+
+// Both of these numbers should be strings
+function calculate(num, total, operation) {
+  total = parseFloat(total);
+  num = parseFloat(num);
+  let result;
+  if (operation == "add") {
+    result = total + num;
+  } else if (operation == "subtract") {
+    result = total - num;
+  }
+
+  console.log(`result: ${result}`);
+  return result;
+};
+
+
+/*
+- when there are 2 numbers and arithmetic buttons are hit such as 2 + 2 ? ; then check what operator was used before, probably with string.includes something, find that operator from before and do the math on that.
+Now 
+
+
+*/
+
+arithmeticBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const btnID = e.currentTarget.dataset.id;
+    console.log(`Total: ${total}, first num: ${userNum}, btnId or operator: ${btnID}`);
+    let mathSymbol;
+    if (btnID == 'mod') {
+      mathSymbol = 'MOD';
+    } else if (btnID == 'divide') {
+      mathSymbol = '/';
+    } else if (btnID == 'multiply') {
+      mathSymbol = 'x';
+    } else if (btnID == 'subtract') {
+      mathSymbol = '-';
+    } else {
+      mathSymbol = '+';
+    }
+    currentOperator = btnID;
+
+    // check if there is no number after then we are just changing the operation sign
+    if (userNum == "") {
+      console.log(equation.slice(0, equation.length - 1));
+      equation = equation.slice(0, equation.length - 1) + mathSymbol;
+    } else { //if there is a number after it which means if firstNum is full then add that sign
+      equation += mathSymbol; 
+      console.log('The other clause was activated');
+    }
+
+    total = userNum;
+    userNum = "";
+
+
+    renderOutput(equation);
+  })
+})
+
+function renderOutput(output) {
+  currentOutputEl.innerHTML = output;
+  // Checks if decimal is in there
+  if (output.includes('.')) {
+    decimalBtn.disabled = true;
+  } else {
+    decimalBtn.disabled = false;
+  }
+}
+decimalBtn.addEventListener('click', function() {
+  equation += '.';
+  renderOutput(equation);
+})
+
+// Clears the equation on screen alongside total and userNum to let user start out fresh.
+clearBtn.addEventListener('click', () => {
+  total = "";
+  userNum = "";
+  equation = "";
+  renderOutput(equation);
+});
+deleteBtn.addEventListener('click', () => {
+  equation = equation.slice(0, equation.length - 1);
+  renderOutput(equation);
+})
+// Puts event listener on the digit buttons so that digits are added on screen
+// We won't the digits into the javascript yet because user may delete or change
+// Some numbers first, after we know they are pressing an arithmetic btn we know 
+// they want to lock that number in, unless its delete. This can be one method
 digitBtns.forEach(btn => {
   btn.addEventListener('click', (e) => {
     const BTN_VALUE = e.currentTarget.dataset.value;
-    firstNum += BTN_VALUE;
+    userNum += BTN_VALUE;
+    equation += BTN_VALUE;
+    renderOutput(equation);
 
-    currentOutputEl.textContent = firstNum;
+
   })
 })
+
