0ef28fed60d61006c55e5e9c0b974fa8dd39874e
Kevin Nguyen <thenoc27@gmail.com>
2022-12-31 19:55:56 -0500

    Got logic for two numbers working

--- app.js
+++ app.js
@@ -12,7 +12,6 @@
 already in the display
 11. Add a backspace
 12. Add keyboard support, be care ful some keys like '/' don't behave properly so event.preventDefault() will be used to solve the problem
-
 + Or something similar
 - Store the first number when the user presses an operator and save the operation that was chosen, then call the operate function when the equal button is pressed.
 Several operations should be able to be done in a row: 12 + 7 - 5 * 3 = 42; okay looks worrying but please wait for the explanation.
@@ -40,75 +39,110 @@ const arithmeticBtns = document.querySelectorAll('.arithmetic-btn');
 // const subtractBtn = document.getElementById('subtraction-btn');
 // const addBtn = document.getElementById('addition-btn');
 
-let userNum = "";
-let total = "";
-let previousOperator;
-let currentOperator;
+let num1 = "";
+let num2 = "";
+
+let operator = "";
 
 // Have the math icons and numbers here for the visual, but 
 // Have the actual numbers and operators in other variables
 let equation = "";
 
-/*
-- Once we press an arithmetic we store that first number. If we plus an arithmetic
-we check if a number has already been stored, if a number was already stored then we 
-know that we should combine the numbers in some way
-*/
 
+// If I add by 0 it displays weird change it
 // Both of these numbers should be strings
-function calculate(num, total, operation) {
-  total = parseFloat(total);
-  num = parseFloat(num);
+function calculate(num1, num2, operation) {
+  console.log(`Calculation trigged num1: ${num1} and num2: ${num2}`);
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   let result;
   if (operation == "add") {
-    result = total + num;
+    result = num1 + num2;
   } else if (operation == "subtract") {
-    result = total - num;
+    result = num1 - num2;
+  } else if (operation == "multiply") {
+    result = num1 * num2;
+  } else if (operation == 'divide') {
+    result = (num1 / num2);
+  } else {
+    result = num1 % num2;
   }
-
-  console.log(`result: ${result}`);
+  // if it isn't an integer then round it; This can happen outside of division so we want a full guard
+  // 9 / 2 is 4.5 then if we multiplied by 0.15 then it would be a very long decimal. Other ways to produce these decimals are possible
+  // So we will stop them here
+  if (!Number.isInteger(result)) {
+    result = result.toFixed(2);
+  }
   return result;
 };
 
 
-/*
-- when there are 2 numbers and arithmetic buttons are hit such as 2 + 2 ? ; then check what operator was used before, probably with string.includes something, find that operator from before and do the math on that.
-Now 
 
+
+/*
+- To my understanding: Let them enter in num1. They are finished entering when operator is selected and set
+the operator. Then switch to the entering to num2; Now if they press equals then they get the answer, however if they press another 
+operator: 
+- Before assigning a new operator, check if the two numbers are full, calculate with the current operator of the 
+two numbers. Get the result and put in num1 and now we can set the next operator
+
+- This seems good in terms of adding in the form of num1 [operator] num2 [new operator]. Obviously we haven't done any thorough error
+handling but it seems good. If it's bad then it's doing a good job hiding it from me. Next would be working on the equal or submit button
 
 */
 
 arithmeticBtns.forEach(btn => {
   btn.addEventListener('click', e => {
     const btnID = e.currentTarget.dataset.id;
-    console.log(`Total: ${total}, first num: ${userNum}, btnId or operator: ${btnID}`);
-    let mathSymbol;
-    if (btnID == 'mod') {
-      mathSymbol = 'MOD';
-    } else if (btnID == 'divide') {
-      mathSymbol = '/';
-    } else if (btnID == 'multiply') {
-      mathSymbol = 'x';
-    } else if (btnID == 'subtract') {
-      mathSymbol = '-';
-    } else {
-      mathSymbol = '+';
+    const mathSymbols = {
+      'mod': 'MOD',
+      'divide': '/',
+      'multiply': 'x',
+      'subtract': '-',
+      'add': '+'
+    };
+    
+    if (num1 == false) {
+      // or a better idea would be leaving everything to an error handling function
+      // if there's no number then that's an error;
+      equation = "Error no number inputted";
+      renderOutput(equation);
+    } else if (operator && num2.length > 0) {
+      // If the operator is already set then you have a 2 + 2 [operator] situation so you have to 
+      // Handle the calculation before reassigning that operator in case user ignores the equal button
+      // remember to empty num2 since it has just been calculated
+      num1 = calculate(num1, num2, operator).toString();
+      num2 = "";      
+      operator = btnID; //set the new [operator]
+      equation = `${num1}${mathSymbols[operator]}`;
+    } else if (operator && (num2 == false)) {         //this would be [number] + [missing number or 0] [new operator]; so a user would press this to change the operator only      
+      console.log(`Second clause triggered since num2: ${num2}`);
+      if (num2 === 0) { //If its a zero then we have a [number] + 0 [new operator];
+        console.log(`num2: ${num2} is detected as zero`);
+        if (operator == 'divide' || 'mod') { //If we are dividing or modulo by zero then its an error
+          equation = `Division Error since ${operator} by 0`;
+          renderOutput(equation);
+        } else { //Else do the regular math 
+          num1 = calculate(num1, num2, operator).toString();
+          num2 = "";
+          operator = btnID; //set the new operator
+          equation = `${num1}${mathSymbols[operator]}`;
+        }
+        
+        //Else if not zero do the regular thing of changing the operator in the logic and visually
+      } else {
+        const LAST_INDEX = equation.length - 1;
+        equation = equation.slice(0, LAST_INDEX); //get rid of old operator could just do .slice(equation.length - 1) since we know operator will be at end of string
+        operator = btnID; //set new operator
+        equation += mathSymbols[operator];
+      }
+      console.log('Second condition clause was hit');
+    } else if (!operator) {
+      operator = btnID;
+      equation += mathSymbols[operator];
     }
-    currentOperator = btnID;
-
-    // check if there is no number after then we are just changing the operation sign
-    if (userNum == "") {
-      console.log(equation.slice(0, equation.length - 1));
-      equation = equation.slice(0, equation.length - 1) + mathSymbol;
-    } else { //if there is a number after it which means if firstNum is full then add that sign
-      equation += mathSymbol; 
-      console.log('The other clause was activated');
-    }
-
-    total = userNum;
-    userNum = "";
-
-
+    
+    
     renderOutput(equation);
   })
 })
@@ -127,10 +161,11 @@ decimalBtn.addEventListener('click', function() {
   renderOutput(equation);
 })
 
-// Clears the equation on screen alongside total and userNum to let user start out fresh.
+// Clears the equation on screen alongside num1 and num2 let user start out fresh.
 clearBtn.addEventListener('click', () => {
-  total = "";
-  userNum = "";
+  num1 = "";
+  num2 = "";
+  operator = "";
   equation = "";
   renderOutput(equation);
 });
@@ -138,18 +173,20 @@ deleteBtn.addEventListener('click', () => {
   equation = equation.slice(0, equation.length - 1);
   renderOutput(equation);
 })
-// Puts event listener on the digit buttons so that digits are added on screen
-// We won't the digits into the javascript yet because user may delete or change
-// Some numbers first, after we know they are pressing an arithmetic btn we know 
-// they want to lock that number in, unless its delete. This can be one method
+
+
 digitBtns.forEach(btn => {
   btn.addEventListener('click', (e) => {
     const BTN_VALUE = e.currentTarget.dataset.value;
-    userNum += BTN_VALUE;
+    
+    // if operator is set then we go to the second number else the first number
+    if (operator) {
+      num2 += BTN_VALUE;
+    } else {
+      num1 += BTN_VALUE;
+    }
+    
     equation += BTN_VALUE;
     renderOutput(equation);
-
-
   })
 })
-
